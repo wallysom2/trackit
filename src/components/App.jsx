d84cd2983b94a216fad4028ba23077a8ef160b55
Wallyson <wallysom2@gmail.com>
2022-08-02 00:20:11 -0300

    pag de login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+
 import Login from "./Login";
+import Cadastro from "./Cadastro";
 
 
 const App = () => {
     return (
-        <div>
-            <Login />
-        </div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/cadastro" element={<Cadastro />} />
+			</Routes>
+		</BrowserRouter>
     );
     }
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,10 @@
+import styled from "styled-components";
+
 import logo from "./../assets/imgs/logo.svg";
 
 const Login = () => {
     return (
-        <div className="login">
+        <LoginStyled>
             <img src={logo} className="login__logo" alt="logo" />
             <form className="login__form">
                 <input type="text" placeholder="Username" />
@@ -10,8 +12,58 @@ const Login = () => {
                 <button type="submit">Login</button>
             </form>
             <h4>Náo tem uma conta? Cadastre-se</h4>
-        </div>
+        </LoginStyled>
     );
 }
 
 export default Login;
+
+const LoginStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color: #f5f5f5;
+    .login__logo {
+        width: 100px;
+        margin-bottom: 20px;
+    }
+    .login__form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        max-width: 400px;
+        padding: 20px;
+        border-radius: 5px;
+
+        input {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            margin-bottom: 10px;
+        }
+        button {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            background-color: #00a8ff;
+            color: #fff;
+
+            &:hover {
+                background-color: #00a8ff;
+                color: #fff;
+            }
+        }
+    }
+    h4 {
+        margin-top: 20px;
+        font-size: 14px;
+        color: #00a8ff;
+        text-align: center;
+    }
+`;
